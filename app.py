4df012e0fb1abfc27d3d6bb6eaae0628beabacd1
Ashley Grant <AshleyGrant@users.noreply.github.com>
2024-05-15 18:31:43 +0000

    S5F1US2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, g
 from database import db, Todo
 from recommendation_engine import RecommendationEngine
+import json
 import os
 
 app = Flask(__name__)
@@ -43,14 +44,33 @@ def remove_todo(id):
     db.session.commit()
     return redirect(url_for("index"))
 
-# show AI recommendations
-@app.route("/recommend/<int:id>", methods=["GET"])
+# Show AI recommendations
+@app.route('/recommend/<int:id>', methods=['GET'])
 async def recommend(id):
     recommendation_engine = RecommendationEngine()
     g.todo = db.session.query(Todo).filter_by(id=id).first()
+
+    if g.todo:
+        try:
+            #attempt to load any saved recommendation from the DB
+            if g.todo.recommendations_json is not None:
+                g.todo.recommendations = json.loads(g.todo.recommendations_json)
+                return render_template('index.html')
+        except ValueError as e:
+            print("Error:", e)
+
     g.todo.recommendations = await recommendation_engine.get_recommendations(g.todo.name)
         
-    return render_template("index.html")
+    # Save the recommendations to the database
+    try:
+        g.todo.recommendations_json = json.dumps(g.todo.recommendations)
+        db.session.add(g.todo)
+        db.session.commit()
+    except Exception as e:
+        print(f"Error adding and committing todo: {e}")
+        return
+
+    return render_template('index.html')
 
 if __name__ == "__main__":
     recommendations = []   
